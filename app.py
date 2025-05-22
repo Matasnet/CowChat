47b1feb12d9c7fb9c643c7ca27ba1014f3a07ab8
MatasNET <matas.wp@gmail.com>
2025-05-22 14:38:51 +0200

    upd

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from cowsay_generator import generate_cowsay
-import os
+import eventlet
+eventlet.monkey_patch()
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='eventlet')  # ważne, aby eventlet
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route("/")
 def index():
@@ -19,5 +20,4 @@ def handle_message(data):
     socketio.send(cow_msg)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=5000)
