00ebad87f017cc2c0ef524ffed6cafd9cec9046e
MatasNET <matas.wp@gmail.com>
2025-05-22 14:35:30 +0200

    Poprawka

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 from cowsay_generator import generate_cowsay
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'tajnehaslo'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, async_mode='eventlet')  # ważne, aby eventlet
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @socketio.on('message')
 def handle_message(data):
-    name = data.get('name')
-    color = data.get('color')
-    text = data.get('text')
-    cow = generate_cowsay(name, text, color)
-    send(cow, broadcast=True)
+    name = data.get("name", "Anonim")
+    message = data.get("message", "")
+    color = data.get("color", "black")
+    cow_msg = generate_cowsay(name, message, color)
+    socketio.send(cow_msg)
 
-if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
